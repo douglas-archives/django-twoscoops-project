512becb9e43fc3a8c675016009a7ebab3b5e4a16
Douglas Miranda <douglasmirandasilva@gmail.com>
2013-04-11 23:59:23 -0400

    my new files and updating requirements

--- project_name/project_name/settings/production.py
+++ project_name/project_name/settings/production.py
@@ -11,14 +11,14 @@ from django.core.exceptions import ImproperlyConfigured
 
 
 def get_env_setting(setting):
-	""" Get the environment setting or return exception """
-	try:
-		return environ[setting]
-	except KeyError:
-		error_msg = "Set the %s env variable" % setting
-		raise ImproperlyConfigured(error_msg)
-		
-INSTALLED_APPS += ('gunicorn',)
+    """ Get the environment setting or return exception """
+    try:
+        return environ[setting]
+    except KeyError:
+        error_msg = "Set the %s env variable" % setting
+        raise ImproperlyConfigured(error_msg)
+
+INSTALLED_APPS += ('gunicorn', 'gevent')
 
 ########## EMAIL CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
---
+++ project_name/start_gunicorn.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source /path/to/.virtualenvs/{{ project_name }}/bin/activate
+# you might want to edit it
+cd ~/webapps/{{ project_name }}
+export DJANGO_SETTINGS_MODULE={{ project_name }}.settings.production
+/home/douglas777/.virtualenvs/{{ project_name }}/bin/python2.7 manage.py run_gunicorn -c etc/gunicorn.production.conf --daemon --settings={{ project_name }}.settings.production && sleep 3
